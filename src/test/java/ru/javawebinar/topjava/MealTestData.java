d8c7b285101ad11d65dc7a05081abd9c28b1f542
polin <polina.suvorova@gmail.com>
2023-06-26 23:42:39 +0300

    HW04 5 check

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -27,18 +27,6 @@ public class MealTestData {
     public static final Meal adminMeal1 = new Meal(ADMIN_MEAL_ID, of(2020, Month.JANUARY, 31, 14, 0), "Админ ланч", 510);
     public static final Meal adminMeal2 = new Meal(ADMIN_MEAL_ID + 1, of(2020, Month.JANUARY, 31, 21, 0), "Админ ужин", 1500);
 
-    static {
-        meal1.setUser(UserTestData.user);
-        meal2.setUser(UserTestData.user);
-        meal3.setUser(UserTestData.user);
-        meal4.setUser(UserTestData.user);
-        meal5.setUser(UserTestData.user);
-        meal6.setUser(UserTestData.user);
-        meal7.setUser(UserTestData.user);
-        adminMeal1.setUser(UserTestData.admin);
-        adminMeal2.setUser(UserTestData.admin);
-    }
-
     public static final List<Meal> meals = Arrays.asList(meal7, meal6, meal5, meal4, meal3, meal2, meal1);
 
     public static Meal getNew() {
