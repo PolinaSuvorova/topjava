2ee5e1ca641e15fce6ece3f1b4dbd43fbab5a536
polin <polina.suvorova@gmail.com>
2023-08-23 22:03:16 +0300

    11_07_HW10_change_locale.patch

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -57,6 +57,8 @@ $(function () {
         }
     });
 
+    $.datetimepicker.setLocale(localeCode);
+
 //  http://xdsoft.net/jqplugins/datetimepicker/
     var startDate = $('#startDate');
     var endDate = $('#endDate');
